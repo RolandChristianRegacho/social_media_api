16801073ad022194cd844ca0c41db134f8a17db5
Roland Regacho <regacho80@gmail.com>
2024-03-19 08:41:43 +0800

    Removed unnecessary functions and file calls

--- api copy/Constant.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("get", "GET");
-define("post", "POST");
-define("put", "PUT");
-define("delete", "DELETE");
-define("max_page_count", 10);
--- api copy/redirect.php
+++
@@ -1,4 +0,0 @@
-<?php
-    if(isset($_GET["code"])) {
-        header("location: ../error_pages/?code=" . $_GET["code"] . "&message=" . $_GET["message"]);
-    }
