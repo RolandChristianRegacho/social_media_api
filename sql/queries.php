9d0700e2c2c8578fdac8f01c20e01e19bb8130e0
Roland Regacho <regacho80@gmail.com>
2024-03-24 18:17:42 +0800

    Fixed a bug in notify reply

--- sql/queries.php
+++ sql/queries.php
@@ -7,7 +7,7 @@ define("getdefaultpic", "SELECT * FROM `images` where `id` = '1'");
 define("getpostbyuserquery", "SELECT `id`, `user`, `content`, `date` FROM `posts` where `user` = ? or `user` = '1' order by `date` desc");
 define("getreplybypostquery", "SELECT `id`, `post_id`, `sender`, `content`, `date` FROM `replies` where `post_id` = ? order by `date` desc");
 define("getpostbyid", "SELECT `id`, `user`, `content`, `date` FROM `posts` where `id` = ?");
-define("getpostuser", "");
+define("getpostuser", "SELECT `user` FROM `posts` where `id` = ?");
 define("getuserinformationbysearchquery", "SELECT `id`, `profile_picture`, `first_name`, `middle_name`, `last_name` FROM `accounts` WHERE first_name LIKE ? or middle_name LIKE ? or last_name LIKE ? ");
 define("getnotificationbyuserquery", "SELECT n.id, n.sender, n.receiver, n.context, n.date, n.status, a.first_name, n.post_id FROM `notifications` n LEFT JOIN `accounts` a ON n.sender = a.id  WHERE `receiver` = ? ORDER BY `date` DESC");
 
